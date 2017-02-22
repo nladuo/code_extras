d0d9c574d54f9f05790332e6145ba250006296d2
nladuo <kalen25115@gmail.com>
2017-02-22 11:07:37 +0800

    modified:   ictlogin.sh

--- ictlogin.sh
+++ ictlogin.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 # 计算所登陆校园网脚本
-# 填写用户名和密码[密码为md5后的字符串的substr(8,16)]
-curl -d "username=&password=&drop=0&type=1&n=100" -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"  http://159.226.39.22/cgi-bin/do_login
+
+username=""
+password=""
+
+pass_salt=$(echo -n '12345678'|md5sum|cut -d ' ' -f1)
+pass_salt=${pass_salt:8:16}
+
+formdata="username="$username"&password="$pass_salt"&drop=0&type=1&n=100"
+
+curl -d $formdata -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"  http://159.226.39.22/cgi-bin/do_login
 echo ""
 curl -d "uid=" http://159.226.39.22/cgi-bin/keeplive
 echo ""
