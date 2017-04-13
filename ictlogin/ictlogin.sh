1090c5894b099aa0a62161db123ba4d27e04f17f
nladuo <kalen25115@gmail.com>
2017-04-13 10:02:10 +0800

    modified:   ictlogin/ictlogin.sh

--- ictlogin/ictlogin.sh
+++ ictlogin/ictlogin.sh
@@ -10,6 +10,7 @@ pass_salt=${pass_salt:8:16}
 
 formdata="username="$username"&password="$pass_salt"&drop=0&type=1&n=100"
 
+echo -n "The Login ID is:"
 curl -d $formdata -H "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36"  http://159.226.39.22/cgi-bin/do_login
 echo ""
 ./infoict.py
